a819ffa4f9a227c58f6c7e0d6dd1653c2d73ea16
Anthony Salgado <anthonyasalgado@gmail.com>
2021-05-11 16:07:37 -0700

    created an app to practice the useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useMemo, useCallback, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ // We create two states that will keep count of the number of time all hooks are called
+ const [callbackCount, setCallbackCount] = useState(0);
+ const [memoCount, setMemoCount] = useState(0);
+
+ const memoFunction = () => {
+   console.log(memoCount, "memo called");
+   // Do something that will take a lot of processing ...
+ };
+
+ // Here if we give an empty array of dependencies, the callback function will return the old value of callbackCount
+ // because useCallback will return its memoized version
+ const callbackFunction = () => {
+   console.log(callbackCount, "callback called");
+   // Do something with callbackCount ...
+   return callbackCount;
+ };
+  // [callbackCount]);
+
+ // We create the memo hook, when memoCount changes, the function will be executed again
+ useMemo(memoFunction, [memoCount]);
+
+ return (
+   <>
+     {/* This component will receive a function that will change when the dependency value changes */}
+     <ChildComponent action={callbackFunction} />
+
+     {/* Change the callback hook dependency to trigger a change in the child */}
+     <button onClick={() => setCallbackCount(callbackCount + 1)}>
+       Change callback count
+     </button>
+
+     {/* After creating useMemo, each change of memoCount will trigger the function passed to the hook,
+   otherwise the memoized value will be returned */}
+     <button onClick={() => setMemoCount(memoCount + 1)}>
+       Change memo count
+     </button>
+   </>
+ );
+};
+
+const ChildComponent = ({action}) => {
+ const [value, setValue] = useState(0)
+
+ useEffect(() => {
+   let val = action()
+   setValue(val)
+ }, [action]) 
+
+ return(
+   <>
+   Child : {value}
+   </>
+ )
 }
 
 export default App;
